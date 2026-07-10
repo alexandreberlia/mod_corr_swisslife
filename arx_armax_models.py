b215cbf2ad79598cef7b57af2948c78595ebb138
alexandreberlia <berliaalexandre@gmail.com>
2026-07-10 16:17:11 +0200

    Update and rename arx_models.py to arx_armax_models.py

--- arx_armax_models.py
+++ arx_armax_models.py
@@ -79,3 +79,73 @@ def predictive_gain(
         "Leading Factor": leading_factor,
         "AIC Gain": gain
     }
+
+m statsmodels.tsa.statespace.sarimax import SARIMAX
+
+
+def compare_arma_vs_leading_indicator(
+    data,
+    target,
+    leading_factor,
+    p,
+    q,
+    lag=1
+):
+    """
+    Compare :
+
+    ARMA(p,q)
+
+    contre
+
+    ARMAX(p,q)
+
+    où le leading_factor est ajouté
+    comme variable explicative.
+    """
+
+    df = data[[target, leading_factor]].copy()
+
+    df[leading_factor] = (
+        df[leading_factor]
+        .shift(lag)
+    )
+
+    df = df.dropna()
+
+    y = df[target]
+
+    x = df[[leading_factor]]
+
+    arma_model = SARIMAX(
+        y,
+        order=(p, 0, q),
+        enforce_stationarity=False,
+        enforce_invertibility=False
+    ).fit(disp=False)
+
+    armax_model = SARIMAX(
+        y,
+        exog=x,
+        order=(p, 0, q),
+        enforce_stationarity=False,
+        enforce_invertibility=False
+    ).fit(disp=False)
+
+    return pd.DataFrame({
+
+        "Model":[
+            f"ARMA({p},{q})",
+            f"ARMAX({p},{q})"
+        ],
+
+        "AIC":[
+            arma_model.aic,
+            armax_model.aic
+        ],
+
+        "BIC":[
+            arma_model.bic,
+            armax_model.bic
+        ]
+    })
