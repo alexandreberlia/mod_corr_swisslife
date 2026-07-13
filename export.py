c2f676cfd4f8ec555e1182d513a275dda18e7182
alexandreberlia <berliaalexandre@gmail.com>
2026-07-13 12:29:09 +0200

    Update export.py

--- export.py
+++ export.py
@@ -1,1 +1,17 @@
+ def export_cleaned_data():
 
+    with pd.ExcelWriter(
+        "cleaned_macro_data.xlsx"
+    ) as writer:
+
+        for name, dataframe in dict_of_df.items():
+
+            dataframe.to_excel(
+                writer,
+                sheet_name=name
+            )
+
+    return "cleaned_macro_data.xlsx"
+
+
+export_cleaned_data()
