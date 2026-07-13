d44e166757072ce432c6c927e800117931c42cc3
alexandreberlia <berliaalexandre@gmail.com>
2026-07-13 15:54:34 +0200

    Update legacy_exploration.py

--- legacy_exploration.py
+++ legacy_exploration.py
@@ -37,39 +37,7 @@
     "### Find the dataframes which match our needs"
    ]
   },
-  {
-   "cell_type": "code",
-   "execution_count": 146,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "list_of_needed_dataframes = ['(GDP)', '(Employment)', '(Inflation)', '(Economic Dynamic)', '(Business Conditions)', '(Housing)', '(Details selling)', '(Household)', '(Customer Trust)']"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 147,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def return_dataframes_with_specific_name______________(name):\n",
-    "    return {df_name:df for df_name, df in dict_of_df.items() if any(name in col for col in df.columns)}\n",
-    "\n",
-    "#return_dataframes_with_specific_name(name='GDP')"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 148,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "def return_columns_with_specific_names______________(name, df):\n",
-    "    return [col for col in df.columns if name in col]\n",
-    "\n",
-    "#return_columns_with_specific_names('GDP', df1)"
-   ]
-  },
+ 
   {
    "cell_type": "markdown",
    "metadata": {},
