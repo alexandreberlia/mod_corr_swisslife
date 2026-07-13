961651ce1f5cd6cea1dae3235799e22fd49274cd
alexandreberlia <berliaalexandre@gmail.com>
2026-07-13 12:33:39 +0200

    Update nettoyage_dataset.py

--- nettoyage_dataset.py
+++ nettoyage_dataset.py
@@ -635,20 +635,3 @@
    ]
   },
 
-   def export_cleaned_data():
-
-    with pd.ExcelWriter(
-        "cleaned_macro_data.xlsx"
-    ) as writer:
-
-        for name, dataframe in dict_of_df.items():
-
-            dataframe.to_excel(
-                writer,
-                sheet_name=name
-            )
-
-    return "cleaned_macro_data.xlsx"
-
-
-export_cleaned_data()
