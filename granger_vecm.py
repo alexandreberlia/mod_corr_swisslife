3e666cd2286f4cb60555d78ae7bb1f4c9b7e4f30
alexandreberlia <berliaalexandre@gmail.com>
2026-07-16 14:09:10 +0200

    Update granger_vecm.py

--- granger_vecm.py
+++ granger_vecm.py
@@ -3,24 +3,24 @@ from statmodels.tsa.vector_ar.vecm import(VECM,select_order,select_coint_rank)
 
 
 def granger_vecm(
-        vecm_results,
+        results,
         significance_level=0.05):
  
-    results = []
+    gc = []
 
-    for caused in consumer_vecm.names:
+    for caused in results.names:
 
-        for causing in consumer_vecm.names:
+        for causing in results.names:
 
             if caused == causing:
                     continue
 
-            test = consumer_vecm.test_granger_causality(
+            test = results.test_granger_causality(
                 caused=caused,
                 causing=causing
             )
 
-            results.append({
+            gc.append({
 
                 "Cause": causing,
 
@@ -34,21 +34,21 @@ def granger_vecm(
 
             })
 
-    granger_df = pd.DataFrame(results)
+    granger_df = pd.DataFrame(gc)
 
     granger_df.sort_values("P-value")
 
 def display_vecm_granger(
-        vecm_results,
+        results,
         significance_level=0.05):
 
-    results = granger_vecm(
-        vecm_results,
+    granger = granger_vecm(
+        results,
         significance_level
     )
 
-    significant = results[
-        results["Granger Causality"]
+    significant = granger[
+        granger["Granger Causality"]
     ]
 
     print()
@@ -62,14 +62,14 @@ def display_vecm_granger(
 
 
 def export_vecm_granger(
-        vecm_results,
+        results,
         excel_name="VECM_Granger"):
 
-    results = granger_vecm(
-        vecm_results
+    granger = granger_vecm(
+        results
     )
 
-    results.to_excel(
+    granger.to_excel(
         f"{excel_name}.xlsx",
         index=False
     )
@@ -78,13 +78,13 @@ def export_vecm_granger(
 
 
 def vecm_granger_matrix(
-        vecm_results):
+        results):
 
-    results = granger_vecm(
-        vecm_results
+    granger = granger_vecm(
+        results
     )
 
-    matrix = results.pivot(
+    matrix = granger.pivot(
         index="Cause",
         columns="Effect",
         values="P-value"
